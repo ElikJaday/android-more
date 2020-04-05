aac031a03b9080f0a2c155b475b6d0190fe92660
Elik Jaday <Tarakan4145@gmail.com>
2020-04-05 16:18:11 +0300

    git init

--- app/src/main/java/dev/elvir/morecommunication/ui/splas_screen/SplashScreenActivity.kt
+++ app/src/main/java/dev/elvir/morecommunication/ui/splas_screen/SplashScreenActivity.kt
@@ -1,18 +1,69 @@
 package dev.elvir.morecommunication.ui.splas_screen
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import dev.elvir.morecommunication.R
-import dev.elvir.morecommunication.ui.sign_in.SignInActivity
+import kotlinx.android.synthetic.main.act_splash_screen.*
+import ua.naiksoftware.stomp.Stomp
+import ua.naiksoftware.stomp.StompClient
+import ua.naiksoftware.stomp.dto.LifecycleEvent
+
 
 class SplashScreenActivity : AppCompatActivity() {
+
+    lateinit var stompClient: StompClient
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.act_splash_screen)
-        Handler().postDelayed({
-            startActivity(Intent(this, SignInActivity::class.java))
-        }, 800)
+        createWebSocketClient();
+        iv_test.setOnClickListener {
+
+        }
+//        Handler().postDelayed({
+//            startActivity(Intent(this, SignInActivity::class.java))
+//        }, 800)
     }
+
+    private fun createWebSocketClient() {
+        stompClient = Stomp.over(
+            Stomp.ConnectionProvider.OKHTTP,
+            "ws://192.168.43.244:9090/example-endpoint/websocket"
+        )
+        stompClient.connect()
+        stompClient.lifecycle().subscribe {
+            when (it.type) {
+                LifecycleEvent.Type.OPENED -> {
+                    Log.i("WebSocket", "Stomp connection opened");
+                }
+                LifecycleEvent.Type.CLOSED -> {
+                    Log.i("WebSocket", "Stomp connection closed");
+                }
+                LifecycleEvent.Type.ERROR -> {
+                    Log.i("WebSocket", "Stomp connection error");
+                    it.exception.printStackTrace()
+                }
+            }
+        }
+        stompClient.topic("/topic/greetings")
+            .subscribe({
+                Log.i("WebSocket", "recieved ${it.payload}")
+            })
+        stompClient.send("/topic/hello-msg-mapping", "My first STOMP message!").subscribe { }
+
+
+    }
+
 }
+
+
+class Chat {
+    var chatId : Long = 123
+    var chatName : String = "undefined"
+    var memberSize : Int = 0
+    var memberId: ArrayList<Long> = arrayListOf(1,2,3)
+    var lastMessage : String = ""
+}
+
